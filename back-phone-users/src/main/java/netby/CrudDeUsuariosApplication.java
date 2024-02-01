fc8046d7b56889e7988d02f9dcd23ee5cab66517
Ronny Gabriel Matute Granizo <rmatute@grupodiosmar.com>
2024-01-31 23:47:47 -0500

    Agregue proyecto vacio de spring-boot - BACK

---
+++ back-phone-users/src/main/java/netby/CrudDeUsuariosApplication.java
@@ -0,0 +1,13 @@
+package netby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDeUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDeUsuariosApplication.class, args);
+	}
+
+}
---
+++ back-phone-users/src/test/java/netby/CrudDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package netby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
